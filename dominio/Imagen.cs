317c20587c1f62d8939146191448804043352816
Santino Gerevini <santinogerevini8@gmail.com>
2026-04-21 17:43:12 -0300

    Change Imagen class from internal to public

--- dominio/Imagen.cs
+++ dominio/Imagen.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace dominio
 {
-    internal class Imagen
+    public class Imagen
     {
         public int IdArticulo { get; set; }
         public int Id {  get; set; }
