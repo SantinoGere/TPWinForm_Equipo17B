56edae37f3756a10d9d35f95a77b63de2b33f5b8
Maximo Gomer <maximogomer02@gmail.com>
2026-04-21 21:55:54 -0300

    Add class AccesoDatos and added namespace to ArticuloNegocio

--- negocio/ArticuloNegocio.cs
+++ negocio/ArticuloNegocio.cs
@@ -5,28 +5,31 @@ using System.Text;
 using System.Threading.Tasks;
 using dominio;
 
-public class ArticuloNegocio
+namespace negocio
 {
-    public List<Articulo> listar()
+    public class ArticuloNegocio
     {
-        List<Articulo> lista = new List<Articulo>();
-        AccesoDatos datos = new AccesoDatos();
-
-        datos.setearConsulta("SELECT A.Id, Codigo, Nombre, A.Descripcion, Precio, M.Descripcion Marca, C.Descripcion Categoria FROM ARTICULOS A, MARCAS M, CATEGORIAS C WHERE A.IdMarca = M.Id AND A.IdCategoria = C.Id");
-
-        datos.ejecutarLectura();
-
-        while (datos.Lector.Read())
+        public List<Articulo> listar()
         {
-            Articulo aux = new Articulo();
-            aux.Id = (int)datos.Lector["Id"];
-            aux.Nombre = (string)datos.Lector["Nombre"];
-            aux.Marca = new Marca();
-            aux.Marca.Descripcion = (string)datos.Lector["Marca"];
+            List<Articulo> lista = new List<Articulo>();
+            AccesoDatos datos = new AccesoDatos();
 
-            lista.Add(aux);
+            datos.setearConsulta("SELECT A.Id, Codigo, Nombre, A.Descripcion, Precio, M.Descripcion Marca, C.Descripcion Categoria FROM ARTICULOS A, MARCAS M, CATEGORIAS C WHERE A.IdMarca = M.Id AND A.IdCategoria = C.Id");
+
+            datos.ejecutarLectura();
+
+            while (datos.Lector.Read())
+            {
+                Articulo aux = new Articulo();
+                aux.Id = (int)datos.Lector["Id"];
+                aux.Nombre = (string)datos.Lector["Nombre"];
+                aux.Marca = new Marca();
+                aux.Marca.Descripcion = (string)datos.Lector["Marca"];
+
+                lista.Add(aux);
+            }
+
+            return lista;
         }
-
-        return lista;
     }
 }
