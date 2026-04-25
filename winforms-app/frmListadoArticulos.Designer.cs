58506e0eacae7d8eace5fa7fc27213a1a7d79d25
Maximo Gomer <maximogomer02@gmail.com>
2026-04-25 11:51:28 -0300

    added buttons to frmListadoArticulos

--- winforms-app/frmListadoArticulos.Designer.cs
+++ winforms-app/frmListadoArticulos.Designer.cs
@@ -26,6 +26,12 @@ namespace winforms_app
             this.lblNombre = new System.Windows.Forms.Label();
             this.lblDescripcion = new System.Windows.Forms.Label();
             this.pbArticulo = new System.Windows.Forms.PictureBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnVerDetalle = new System.Windows.Forms.Button();
+            this.lblFiltradoRapido = new System.Windows.Forms.Label();
+            this.txtFiltradoRapido = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dgvArticulos)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbArticulo)).BeginInit();
             this.SuspendLayout();
@@ -33,16 +39,18 @@ namespace winforms_app
             // dgvArticulos
             // 
             this.dgvArticulos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvArticulos.Location = new System.Drawing.Point(12, 12);
+            this.dgvArticulos.Location = new System.Drawing.Point(13, 38);
             this.dgvArticulos.Name = "dgvArticulos";
-            this.dgvArticulos.Size = new System.Drawing.Size(560, 337);
+            this.dgvArticulos.ReadOnly = true;
+            this.dgvArticulos.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvArticulos.Size = new System.Drawing.Size(560, 263);
             this.dgvArticulos.TabIndex = 0;
             this.dgvArticulos.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvArticulos_CellContentClick);
             // 
             // lblNombre
             // 
             this.lblNombre.AutoSize = true;
-            this.lblNombre.Location = new System.Drawing.Point(303, 18);
+            this.lblNombre.Location = new System.Drawing.Point(745, 46);
             this.lblNombre.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
             this.lblNombre.Name = "lblNombre";
             this.lblNombre.Size = new System.Drawing.Size(44, 13);
@@ -52,7 +60,7 @@ namespace winforms_app
             // lblDescripcion
             // 
             this.lblDescripcion.AutoSize = true;
-            this.lblDescripcion.Location = new System.Drawing.Point(303, 28);
+            this.lblDescripcion.Location = new System.Drawing.Point(745, 59);
             this.lblDescripcion.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
             this.lblDescripcion.Name = "lblDescripcion";
             this.lblDescripcion.Size = new System.Drawing.Size(63, 13);
@@ -61,19 +69,77 @@ namespace winforms_app
             // 
             // pbArticulo
             // 
-            this.pbArticulo.Location = new System.Drawing.Point(303, 39);
-            this.pbArticulo.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.pbArticulo.Location = new System.Drawing.Point(748, 73);
+            this.pbArticulo.Margin = new System.Windows.Forms.Padding(1);
             this.pbArticulo.Name = "pbArticulo";
             this.pbArticulo.Size = new System.Drawing.Size(79, 70);
             this.pbArticulo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pbArticulo.TabIndex = 3;
             this.pbArticulo.TabStop = false;
             // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(13, 307);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 4;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Location = new System.Drawing.Point(94, 307);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(75, 23);
+            this.btnModificar.TabIndex = 5;
+            this.btnModificar.Text = "Modificar";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(175, 307);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 6;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // btnVerDetalle
+            // 
+            this.btnVerDetalle.Location = new System.Drawing.Point(256, 307);
+            this.btnVerDetalle.Name = "btnVerDetalle";
+            this.btnVerDetalle.Size = new System.Drawing.Size(75, 23);
+            this.btnVerDetalle.TabIndex = 7;
+            this.btnVerDetalle.Text = "Ver Detalle";
+            this.btnVerDetalle.UseVisualStyleBackColor = true;
+            // 
+            // lblFiltradoRapido
+            // 
+            this.lblFiltradoRapido.AutoSize = true;
+            this.lblFiltradoRapido.Location = new System.Drawing.Point(13, 13);
+            this.lblFiltradoRapido.Name = "lblFiltradoRapido";
+            this.lblFiltradoRapido.Size = new System.Drawing.Size(78, 13);
+            this.lblFiltradoRapido.TabIndex = 8;
+            this.lblFiltradoRapido.Text = "Filtrado Rapido";
+            // 
+            // txtFiltradoRapido
+            // 
+            this.txtFiltradoRapido.Location = new System.Drawing.Point(94, 10);
+            this.txtFiltradoRapido.Name = "txtFiltradoRapido";
+            this.txtFiltradoRapido.Size = new System.Drawing.Size(282, 20);
+            this.txtFiltradoRapido.TabIndex = 9;
+            // 
             // frmListadoArticulos
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(585, 363);
+            this.ClientSize = new System.Drawing.Size(875, 363);
+            this.Controls.Add(this.txtFiltradoRapido);
+            this.Controls.Add(this.lblFiltradoRapido);
+            this.Controls.Add(this.btnVerDetalle);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnModificar);
+            this.Controls.Add(this.btnAgregar);
             this.Controls.Add(this.dgvArticulos);
             this.Controls.Add(this.lblNombre);
             this.Controls.Add(this.lblDescripcion);
@@ -89,5 +155,12 @@ namespace winforms_app
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnVerDetalle;
+        private System.Windows.Forms.Label lblFiltradoRapido;
+        private System.Windows.Forms.TextBox txtFiltradoRapido;
     }
 }
